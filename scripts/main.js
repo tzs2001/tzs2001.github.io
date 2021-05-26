e8d76da8e9f59b2e1d5fe5319fd67ecc97e549e1
tzs2001 <84113308+tzs2001@users.noreply.github.com>
2021-05-26 21:53:03 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
+window.onload = function() {
+    const webDesign = document.querySelector('section#webDesign a');
+    const text = 'https://www...'.split('');
 
+    let index = 0;
+    function add() {
+        if (index < text.length) {
+            webDesign.textContent += text[index++];
+        } else {
+            webDesign.textContent = 'h';
+            index = 1;
+        }
+    }
+    setInterval(add, 400);
+}
